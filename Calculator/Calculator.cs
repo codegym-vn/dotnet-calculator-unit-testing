22462fc36b0df3ced257afcfddabaf2f196d8b76
nguyenkhanhtung <tungnk0711@gmail.com>
2020-03-09 14:10:06 +0700

    commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            if (x / 2 + y / 2 >= int.MaxValue / 2)
+            {
+                throw new Exception("out of range exception");
+            }
+
+            if (x / 2 + y / 2 <= int.MinValue / 2)
+            {
+                throw new Exception("out of range exception");
+            }
+            
+            return x + y;
+        }
+
+    }
+}
